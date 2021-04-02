ffc4c35191a4fbfdac3c32e88911242928efc322
shota <kawano@r102.co.jp>
2021-04-02 13:09:47 +0900

    初期化

---
+++ app/src/main/java/jp/microvent/microvent/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package jp.microvent.microvent.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import jp.microvent.microvent.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
